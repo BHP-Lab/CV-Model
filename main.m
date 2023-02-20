6bd80834fdc6673d72c69dd489065b6ba240a5f6
rswhittle <rswhittle@tamu.edu>
2023-02-20 16:30:35 -0600

    Initial commit for the head model branch

--- main.m
+++ main.m
@@ -1,16 +1,7 @@
 clear; close all;
 load input_reflex_filters.mat
-sim cv_model_50F
+sim cv_model
 process_output
 calculate_trends
 plot_graphs
-save(strcat('Omnibus_50F'));
-
-clear; close all;
-load input_reflex_filters.mat
-sim cv_model_50M
-process_output
-calculate_trends
-plot_graphs
-save(strcat('Omnibus_50M'));
-
+%save(strcat('Omnibus'));
